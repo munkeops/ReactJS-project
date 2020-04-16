7285762165556d071051924a1902886f0991588a
rohan9025 <nrgoun@gmail.com>
2020-04-16 20:38:20 +0530

    working buttons

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import logo from './logo.svg';
 import './App.css';
+// import { ReactComponent } from '*.svg';
 
-
+//{()=>{window.open(this.props.url)}}>
 class NavButton extends React.Component{
+
+  
+  
+    // constructor(props){
+    //   super(props)
+    //   this.state={
+    //     name:this.props.name,
+    //     link:this.props.link,
+    //     func:this.props.changeView
+    //   }
+    // }
+  
+  click(){
+    this.props.changeView(this.props.name)
+  }
   render(){
-    console.log("hey")
+    console.log("hey");
+    
     return(
-      <button className="button" onClick={()=>{window.open(this.props.url)}}>
+      <button className="button" onClick= {this.click.bind(this)}>   
         {this.props.name}
       </button>
    )
@@ -17,13 +34,19 @@ class NavButton extends React.Component{
 
 
 class Navbar extends React.Component{
+
+  // constructor(props){
+  //   super(props);
+
+  // }
   
   renderButton(namebut,link){
     
     return(
         <NavButton
-        name={namebut}
-        url={link}
+          name={namebut}
+          url={link}
+          changeView={this.props.changeView}
         />
 
     )
@@ -45,23 +68,108 @@ class Navbar extends React.Component{
   }
 }
 class SideBar extends React.Component{
+  
   render(){
-    return(
-      <div style={{width:"20%"}} className="SideMenu">
+      return(
+        <div style={{width:"20%"}} className="SideMenu">
 
-      </div>
-    )
-    
+        </div>
+    )    
   }
 }
+
+class Home extends React.Component{
+
+  render(){
+    return(
+      <div>
+        <SideBar/>
+      </div>
+    )
+  }
+}
+class About extends React.Component{
+  render(){
+    return(
+      <div>
+        <p align='center'>About Page</p>
+      </div>
+    )
+  }
+}
+
+class Movies extends React.Component{
+  render(){
+    return(
+      <div>
+        <p align='center'>Movies Page</p>
+      </div>
+    )
+  }
+}
+
+class Contact extends React.Component{
+  render(){
+    return(
+      <div>
+        <p align='center'>Contact Page</p>
+      </div>
+    )
+  }
+}
+
+// class HomePage extends React.Component{
+//   constructor(props){
+//     super(props)
+//   }
+// }
 class Page extends React.Component{
 
+  constructor(props){
+    super(props)
+    this.state={
+      view:'HOME'
+    }
+    this.changeView=this.changeView.bind(this)
+  }
+  
+
+  changeView(viewnum){
+    
+    this.setState({
+      view: viewnum
+    })
+
+  }
+
+  display(){
+
+    if(this.state.view==='HOME')
+    {
+      return <Home/>
+    }
+    else if(this.state.view==='ABOUT')
+    {
+      return <About/>
+    }
+    else if(this.state.view==='MOVIES')
+    {
+      return <Movies/>
+    }
+    else if(this.state.view==='CONTACT')
+    {
+      return <Contact/>
+    }
+  }
+  
+  
   render(){
     return(
       <div className="row" >
-          <Navbar/>
+          <Navbar changeView={this.changeView}/>
           <div className="Bodydiv">
-            <SideBar/>
+            {this.display()}
+            
           </div>
       </div>
     )
